2378e5d8a3bf7a2f87787aaf9caba972c907f727
SadRobotChannel <46029311+SadRobotChannel@users.noreply.github.com>
2019-03-01 13:50:56 +0100

    Add files via upload

--- all/js/index.js
+++ all/js/index.js
@@ -4,13 +4,13 @@ angular.module('sortApp', [])
   $scope.sortType     = 'name'; // set the default sort type
   $scope.sortReverse  = false;  // set the default sort order
   $scope.searchFish   = '';     // set the default search/filter term
-  
-  // create the list of sushi rolls 
+
+  // create the list of sushi rolls
   $scope.sushi = [
-    { name: 'Cali Roll', fish: 'Crab', tastiness: 2 },
-    { name: 'Philly', fish: 'Tuna', tastiness: 4 },
-    { name: 'Tiger', fish: 'Eel', tastiness: 7 },
-    { name: 'Rainbow', fish: 'Variety', tastiness: 6 }
+    { name: 'Cali Roll', fish: 'Crab', frequency: 2 },
+    { name: 'Philly', fish: 'Tuna', frequency: 4 },
+    { name: 'Tiger', fish: 'Eel', frequency: 7 },
+    { name: 'Rainbow', fish: 'Variety', frequency: 6 }
   ];
-  
+
 });
