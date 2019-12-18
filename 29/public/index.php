327241fdc026ac0e0b5fc2d78340d10dc0fedd42
SadRobotChannel <46029311+SadRobotChannel@users.noreply.github.com>
2019-12-18 22:23:02 +0100

    Add files via upload

---
+++ 29/public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ +------------------------------------------------------------------------+
+ | Phosphorum                                                             |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2013-present Phalcon Team and contributors               |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file LICENSE.txt.                             |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to license@phalconphp.com so we can send you a copy immediately.       |
+ +------------------------------------------------------------------------+
+*/
+
+use Phosphorum\Bootstrap;
+
+// Register the auto loader
+require __DIR__.'/../bootstrap/autoloader.php';
+
+$bootstrap = new Bootstrap();
+
+echo $bootstrap->run();
